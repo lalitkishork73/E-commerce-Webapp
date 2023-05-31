23630922d39066f51cc69bbee41f7677c726d483
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-05-31 19:00:17 +0530

    updated

--- server/build/server.js
+++ server/build/server.js
@@ -5,11 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongonect_1 = require("./configs/mongonect");
+const mongoose_1 = __importDefault(require("mongoose"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const router = express_1.default.Router();
-router.get('/', function (req, res) {
-    res.send("All systems operational");
-});
 app.use((0, cors_1.default)());
-app.use(router);
-app.listen(3000, () => console.log('Listening on port 3000 lalit'));
+mongoose_1.default.set('strictQuery', false);
+exports.default = mongoose_1.default
+    .connect(mongonect_1.config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+    console.log('Mongodb connected');
+})
+    .catch((err) => console.log(err));
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'));
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
-import cors from 'cors'; 
-import dotenv from 'dotenv';
-dotenv.config();
+import cors from 'cors';
+import { config } from './configs/mongonect';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors());
+mongoose.set('strictQuery', false);
+export default mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        console.log('Mongodb connected');
+    })
+    .catch((err) => console.log(err));
 
 
-
-
-app.listen(process.env.PORT||3000, () => console.log('Listening on port 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'));
